8f8864484d10c15526f1da65413f920aa152d767
Akunuri Roshini <roshini.akunuri@acsicorp.com>
2022-09-06 16:21:13 +0530

    intial

---
+++ app/views/articles/full_messages_for.rb
@@ -0,0 +1,13 @@
+def new
+    @article = Article.new
+  end
+
+  def create
+    @article = Article.new(article_params)
+
+    if @article.save
+      redirect_to @article
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
